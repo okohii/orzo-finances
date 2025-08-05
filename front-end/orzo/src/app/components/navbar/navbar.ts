55c828a992b448a4004b38e0363428b6a0946cb8
Gustavo Gil <gustavo.gil@sptech.school>
2025-08-05 10:12:16 -0300

    animações navbar

--- front-end/orzo/src/app/components/navbar/navbar.ts
+++ front-end/orzo/src/app/components/navbar/navbar.ts
@@ -3,23 +3,31 @@ import { NavbarService } from '../../services/navbar.service';
 import { ThemeButton } from '../theme-button/theme-button';
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ThemeButton, RouterModule, MatIconModule],
+  imports: [ThemeButton, RouterModule, MatIconModule, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
   private navbarService = inject(NavbarService);
+
+  navItemsClass = 'nav-items';
   
   get isExpanded() {
     return this.navbarService.isExpanded();
   }
   
   toggleSidebar() {
+    if (this.isExpanded) {
+      setTimeout(() => this.navItemsClass = 'nav-items collapsing', 100);
+    } else {
+      this.navItemsClass = 'nav-items';
+    }
     this.navbarService.toggle();
   }
 }
