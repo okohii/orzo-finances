58b82175dedfddf26564f1cd74da74465426ba80
Gustavo Gil <gustavo.gil@sptech.school>
2025-08-06 14:27:42 -0300

    backend

---
+++ back-end/orzo/src/main/java/orzo/orzo_finances/OrzoFinancesApplication.java
@@ -0,0 +1,13 @@
+package orzo.orzo_finances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrzoFinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrzoFinancesApplication.class, args);
+	}
+
+}
---
+++ back-end/orzo/src/test/java/orzo/orzo_finances/OrzoFinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package orzo.orzo_finances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrzoFinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
